a7f1509e5b9e5d1667936d20c0bdc41a73fac4a6
Hamsterbil <gunnarmani04@gmail.com>
2025-04-21 16:58:04 +0200

    Small fix

--- app/node/PublicResources/js/client.js
+++ app/node/PublicResources/js/client.js
@@ -37,57 +37,6 @@ function jsonPost(url = '', data={}) {
   return fetch(url, options).then(jsonParse);
 }
 
-//some helper functions to show/hide/enable/diable elements of the HTML page
-function hideElem(elem) {
-  elem.style.visibility="hidden";
-}
-
-function showElem(elem) {
- elem.style.visibility="visible";
-}
-function disableElem(elem) {
-  elem.style.display="none";
-}
-//assumes the element should be a block type
-function enableElem(elem) {
- elem.style.display="block";
-}
-
-//Menu button elements have an id of the form: xxxx_Btn_id 
-//A corresponding section has an id of the form: xxxx_Section_id 
-//This function map from menu button id to section id 
-function menuBtnId2SectionId(btnId) {
- const featureName=btnId.split("_Btn_id")[0]; //remove _Btn_id suffix, eg "record"
- return featureName+"_Section_id";            //eg record_Section_id 
-}
-
-//eventhandler that is called when a menu button is clicked.
-//it shows the corresponding section containing the requested feature
-function selectFeature(event) {
-  let selectedFeature=event.target.id; //recordBtn,statsBtn, or helpBtn
-  let featureNameId=menuBtnId2SectionId(selectedFeature); 
-  let featureElem=document.querySelector("#"+featureNameId); 
-  hideFeatures();         //Hide all feature sections
-  enableElem(featureElem);//and show only selected feature
-  console.log("selected "+featureNameId);
-}
-
-//register menu event handler functions
-function registerMenuSelect() {
-  let menuBtns=document.querySelectorAll("nav button"); //get all buttons in nav element containing the "menu"
-  for(let btn of menuBtns) 
-    btn.addEventListener("click",selectFeature); 
-}
-
-//hide all program feature (corresponding to HTML sections)
-function hideFeatures() {
-  let menuBtns=document.querySelectorAll("nav button"); //get all buttons in nav element containing the "menu"
-  for(let btn of menuBtns) {
-    let featureNameId=menuBtnId2SectionId(btn.id); 
-    disableElem(document.querySelector("#"+featureNameId));
-  }
-}
-
 function loginForm() {
   document.getElementById('loginForm').addEventListener('submit', async function (e) {
     e.preventDefault();
